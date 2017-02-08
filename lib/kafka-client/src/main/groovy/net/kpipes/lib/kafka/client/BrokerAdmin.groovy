a3c9080358010966a028dc22a448b181b08cf438
Henryk Konsek <hekonsek@gmail.com>
2017-02-08 21:14:52 +0100

    Handled double topic creation.

--- lib/kafka-client/src/main/groovy/net/kpipes/lib/kafka/client/BrokerAdmin.groovy
+++ lib/kafka-client/src/main/groovy/net/kpipes/lib/kafka/client/BrokerAdmin.groovy
@@ -5,8 +5,14 @@ import kafka.admin.RackAwareMode
 import kafka.utils.ZKStringSerializer$
 import kafka.utils.ZkUtils
 import org.I0Itec.zkclient.ZkClient
+import org.apache.kafka.common.errors.TopicExistsException
+import org.slf4j.Logger
+
+import static org.slf4j.LoggerFactory.getLogger
 
 class BrokerAdmin {
+
+    private static final Logger LOG = getLogger(BrokerAdmin)
 
     private final String zooKeeperHost
 
@@ -22,10 +28,14 @@ class BrokerAdmin {
         def zkClient = new ZkClient("${zooKeeperHost}:${zooKeeperPort}", Integer.MAX_VALUE, 10000, ZKStringSerializer$.MODULE$)
         ZkUtils zooKeeperUtils = ZkUtils.apply(zkClient, false)
 
-        if (!AdminUtils.topicExists(zooKeeperUtils, topic)) {
-            RackAwareMode mode = RackAwareMode.Disabled$.MODULE$
-            AdminUtils.createTopic(zooKeeperUtils, topic, 25, 1, new Properties(), mode)
-            Thread.sleep(15000)
+        try {
+            if (!AdminUtils.topicExists(zooKeeperUtils, topic)) {
+                RackAwareMode mode = RackAwareMode.Disabled$.MODULE$
+                AdminUtils.createTopic(zooKeeperUtils, topic, 25, 1, new Properties(), mode)
+                Thread.sleep(15000)
+            }
+        } catch (TopicExistsException e) {
+            LOG.debug(e.message)
         }
     }
 
