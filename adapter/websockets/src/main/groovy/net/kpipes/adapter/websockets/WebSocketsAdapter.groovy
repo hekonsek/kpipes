5894c4498c6006d1d07e0818ed511dbfe1c60d84
Henryk Konsek <hekonsek@gmail.com>
2017-02-16 17:53:06 +0100

    Refactoring.

--- adapter/websockets/src/main/groovy/net/kpipes/adapter/websockets/WebSocketsAdapter.groovy
+++ adapter/websockets/src/main/groovy/net/kpipes/adapter/websockets/WebSocketsAdapter.groovy
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the KPipes under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.kpipes.adapter.websockets
 
 import io.vertx.core.Vertx
@@ -28,17 +44,20 @@ class WebSocketsAdapter {
         this.kafkaPort = kafkaPort
     }
 
+    // Life-cycle
+
     void start() {
         Vertx.vertx().createHttpServer().websocketHandler { ws ->
-            if(ws.uri().startsWith('/event/')) {
-                def eventName = ws.uri().replaceFirst(/\/event\//, '')
+            def uri = ws.uri()
+            if(uri.startsWith('/event/')) {
+                def eventName = uri.replaceFirst(/\/event\//, '')
                 brokerAdmin.ensureTopicExists(eventName)
                 ws.handler { message ->
                     def key = ws.headers().get('kpipes.key')
                     kafkaProducer.send(new ProducerRecord(eventName, key ?: uuid(), new Bytes(message.bytes)))
                 }
-            } else if(ws.uri().startsWith('/notification/')) {
-                def channelName = ws.uri().replaceFirst(/\/notification\//, '')
+            } else if(uri.startsWith('/notification/')) {
+                def channelName = uri.replaceFirst(/\/notification\//, '')
                 def channel = "notification.${channelName}"
                 brokerAdmin.ensureTopicExists(channel) // TODO kafkaConsumerTemplate.subscribe should create topic
                 kafkaConsumerTemplate.subscribe(new KafkaConsumerBuilder<>(uuid()).port(kafkaPort).build(), channel) {
