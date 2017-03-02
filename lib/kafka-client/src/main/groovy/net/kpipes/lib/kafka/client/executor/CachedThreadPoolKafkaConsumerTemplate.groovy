efa335ae90cd108953650434107d098f50aeb8f6
Henryk Konsek <hekonsek@gmail.com>
2017-03-02 18:24:31 +0100

    Consumer template should support wake up calls.

--- lib/kafka-client/src/main/groovy/net/kpipes/lib/kafka/client/executor/CachedThreadPoolKafkaConsumerTemplate.groovy
+++ lib/kafka-client/src/main/groovy/net/kpipes/lib/kafka/client/executor/CachedThreadPoolKafkaConsumerTemplate.groovy
@@ -1,15 +1,20 @@
 package net.kpipes.lib.kafka.client.executor
 
+import groovy.transform.CompileStatic
 import net.kpipes.lib.kafka.client.BrokerAdmin
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.errors.WakeupException
+
+import java.util.concurrent.ExecutorService
 
 import static java.util.concurrent.Executors.newCachedThreadPool
 
+@CompileStatic
 class CachedThreadPoolKafkaConsumerTemplate implements KafkaConsumerTemplate {
 
     private final BrokerAdmin brokerAdmin
 
-    private final executor = newCachedThreadPool()
+    private final ExecutorService executor = newCachedThreadPool()
 
     CachedThreadPoolKafkaConsumerTemplate(BrokerAdmin brokerAdmin) {
         this.brokerAdmin = brokerAdmin
@@ -18,10 +23,16 @@ class CachedThreadPoolKafkaConsumerTemplate implements KafkaConsumerTemplate {
     @Override
     def <K, V> void consumeRecords(KafkaConsumer<K, V> consumer, ConsumerRecordsCallback<K, V> consumerRecordsCallback) {
         executor.submit {
-            while (true) {
-                def events = consumer.poll(5000)
-                consumerRecordsCallback.onConsumerRecords(events)
-                Thread.sleep(100)
+            boolean isRunning = true
+            while (isRunning) {
+                try {
+                    def events = consumer.poll(5000)
+                    consumerRecordsCallback.onConsumerRecords(events)
+                    Thread.sleep(100)
+                } catch (WakeupException e) {
+                    isRunning = false
+                    consumer.close()
+                }
             }
         }
     }
