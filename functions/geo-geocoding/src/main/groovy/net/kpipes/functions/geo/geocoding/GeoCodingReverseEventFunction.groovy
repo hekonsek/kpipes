88a6ced99309bdfc97add967f396f43186c42610
Henryk Konsek <hekonsek@gmail.com>
2017-03-02 19:18:16 +0100

    Geocoding function should give up connecting to Google Services after 5 seconds.

--- functions/geo-geocoding/src/main/groovy/net/kpipes/functions/geo/geocoding/GeoCodingReverseEventFunction.groovy
+++ functions/geo-geocoding/src/main/groovy/net/kpipes/functions/geo/geocoding/GeoCodingReverseEventFunction.groovy
@@ -5,7 +5,15 @@ import net.kpipes.core.function.Event
 import net.kpipes.core.function.EventMappingFunction
 import org.apache.camel.ProducerTemplate
 
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeoutException
+
+import static java.util.concurrent.TimeUnit.SECONDS
+
 class GeoCodingReverseEventFunction implements EventMappingFunction {
+
+    private final executor = Executors.newCachedThreadPool()
 
     private final ProducerTemplate camelProducerTemplate
 
@@ -15,10 +23,16 @@ class GeoCodingReverseEventFunction implements EventMappingFunction {
 
     @Override
     Map<String, Object> onEvent(Event event) {
-        def geocodeResponse = camelProducerTemplate.requestBody("geocoder:latlng:${event.body().lat},${event.body().lng}", null, GeocodeResponse)
-        def address = geocodeResponse.results.first().formattedAddress
-        def city = geocodeResponse.results.find { it.types.contains('locality') }.addressComponents.first().longName
-        event.body()['response.geo.coding.reverse'] = [address: address, city: city]
+        try {
+            def geocodeResponse = executor.submit({
+                camelProducerTemplate.requestBody("geocoder:latlng:${event.body().lat},${event.body().lng}", null, GeocodeResponse)
+            } as Callable<GeocodeResponse>).get(5, SECONDS)
+            def address = geocodeResponse.results.first().formattedAddress
+            def city = geocodeResponse.results.find { it.types.contains('locality') }.addressComponents.first().longName
+            event.body()['response.geo.coding.reverse'] = [address: address, city: city]
+        } catch (TimeoutException e) {
+            event.body()['response.geo.coding.reverse'] = [error: 'Connection timeout']
+        }
         event.body()
     }
 
