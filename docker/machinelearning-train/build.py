3b161882ceb6c3a7ea80ab5abeb1a371c20bacf5
Henryk Konsek <hekonsek@gmail.com>
2017-03-24 11:16:41 +0100

    Fixed broken build.

--- docker/machinelearning-train/build.py
+++ docker/machinelearning-train/build.py
@@ -2,8 +2,8 @@ import os
 pwd = os.getcwd()
 
 from subprocess import call
-resultCode = call(["docker", "run", "-v", pwd + "/src/test/data:/data", "-v", "/tmp/kpipesmlstate:/model", "-e", "MODEL_ID=mymodel", "-e", "DATA_ID=numeric", "kpipes/service-machinelearning-train"])
+resultCode = call(["docker", "run", "-v", pwd + "/src/test/data:/data", "-v", "/tmp/kpipesmlstate:/model", "-e", "MODEL_ID=mymodel", "-e", "DATA_ID=numeric", "kpipes/machinelearning-train"])
 assert resultCode == 0
 
-resultCode = call(["docker", "run", "-v", pwd + "/src/test/data:/data", "-v", "/tmp/kpipesmlmodel:/model", "-e", "MODEL_ID=mymodel", "-e", "DATA_ID=text", "kpipes/service-machinelearning-train"])
+resultCode = call(["docker", "run", "-v", pwd + "/src/test/data:/data", "-v", "/tmp/kpipesmlmodel:/model", "-e", "MODEL_ID=mymodel", "-e", "DATA_ID=text", "kpipes/machinelearning-train"])
 assert resultCode == 0
